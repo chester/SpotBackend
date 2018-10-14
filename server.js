5d8d2eaeee3792c2b3ca87c3aed81e403a53cb1d
Chester Vanhuang <cvanhuan@ucsd.edu>
2018-10-14 04:20:06 -0700

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+//API Routes
+
+
+var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+console.log('port is', port);
+});
+
